fde6d7da8c4bf1865a9fb9fe203f94d606fbc669
Anekoique <ctolu01@gmail.com>
2025-07-24 19:47:06 +0800

    feat: update

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (int i = 0; i < 4; i++) {
+            shape[i] = shape_[i];
+            size *= shape_[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -21,13 +25,50 @@ struct Tensor4D {
     Tensor4D(Tensor4D const &) = delete;
     Tensor4D(Tensor4D &&) noexcept = delete;
 
-    // 这个加法需要支持“单向广播”。
+    void linear_to_coords(unsigned int linear_idx, unsigned int const shape_[4], unsigned int coords[4]) const {
+        coords[3] = linear_idx % shape_[3];
+        linear_idx /= shape_[3];
+        coords[2] = linear_idx % shape_[2];
+        linear_idx /= shape_[2];
+        coords[1] = linear_idx % shape_[1];
+        linear_idx /= shape_[1];
+        coords[0] = linear_idx;
+    }
+    
+    unsigned int coords_to_linear(unsigned int const coords[4], unsigned int const shape_[4]) const {
+        return coords[0] * shape_[1] * shape_[2] * shape_[3] +
+               coords[1] * shape_[2] * shape_[3] +
+               coords[2] * shape_[3] +
+               coords[3];
+    }
+    
+    // 这个加法需要支持"单向广播"。
     // 具体来说，`others` 可以具有与 `this` 不同的形状，形状不同的维度长度必须为 1。
     // `others` 长度为 1 但 `this` 长度不为 1 的维度将发生广播计算。
     // 例如，`this` 形状为 `[1, 2, 3, 4]`，`others` 形状为 `[1, 2, 1, 4]`，
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        unsigned int total_size = shape[0] * shape[1] * shape[2] * shape[3];
+        
+        for (unsigned int i = 0; i < total_size; i++) {
+            unsigned int this_coords[4];
+            linear_to_coords(i, shape, this_coords);
+            
+            unsigned int other_coords[4];
+            for (int d = 0; d < 4; d++) {
+                if (others.shape[d] == 1) {
+                    other_coords[d] = 0;
+                } else {
+                    other_coords[d] = this_coords[d];
+                }
+            }
+            
+            unsigned int other_idx = coords_to_linear(other_coords, others.shape);
+            
+            data[i] += others.data[other_idx];
+        }
+        
         return *this;
     }
 };
